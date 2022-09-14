cf5b902e0449913bc8f99d820d50c167f9dee91b
Rajie04 <rtahmed04@gmail.com>
2022-09-13 23:39:10 -0500

    create weather app skeleton with api fetch

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+
+api_key = 'd084d3ea1a9aa98ebcf26383957e6a97'
+#get user input
+userInput = input('Enter City: ')
+
+#fetch weather data
+weatherData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={userInput}&units=imperial&APPID={api_key}")
+
+#choose which data to display
+weather = weatherData.json()['weather'][0]['main']
+temp = round(weatherData.json()['main']['temp'])
+tempMin = round(weatherData.json()['main']['temp_min'])
+tempMax = round(weatherData.json()['main']['temp_max'])
+
+print(f'The weather in {userInput} is {weather}')
+print(f'The temperature is {temp}°F, with a high of {tempMax}°F and a low of {tempMin}°F.')
