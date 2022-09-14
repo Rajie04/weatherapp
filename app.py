359ecfe0649b90d2b6f6f6b6205e2b5299b71f9f
Rajie04 <rtahmed04@gmail.com>
2022-09-13 23:52:50 -0500

    add error message for no city found

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ userInput = input('Enter City: ')
 
 #fetch weather data
 weatherData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={userInput}&units=imperial&APPID={api_key}")
+if weatherData.json()['cod'] == '404':
+    print("No City Found")
+else: 
+    #choose which data to display
+    weather = weatherData.json()['weather'][0]['main']
+    temp = round(weatherData.json()['main']['temp'])
+    tempMin = round(weatherData.json()['main']['temp_min'])
+    tempMax = round(weatherData.json()['main']['temp_max'])
 
-#choose which data to display
-weather = weatherData.json()['weather'][0]['main']
-temp = round(weatherData.json()['main']['temp'])
-tempMin = round(weatherData.json()['main']['temp_min'])
-tempMax = round(weatherData.json()['main']['temp_max'])
-
-print(f'The weather in {userInput} is {weather}')
-print(f'The temperature is {temp}°F, with a high of {tempMax}°F and a low of {tempMin}°F.')
+    print(f'The weather in {userInput} is {weather}')
+    print(f'The temperature is {temp}°F, with a high of {tempMax}°F and a low of {tempMin}°F.')
